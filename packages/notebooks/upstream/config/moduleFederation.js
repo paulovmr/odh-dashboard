ca174399c4e315bd8457671b4d25d9bf7ff8227e
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-08-29 16:40:15 -0300

    added notebooks folder and initial files
    Signed-off-by: paulovmr <832830+paulovmr@users.noreply.github.com>

--- packages/notebooks/upstream/config/moduleFederation.js
+++ packages/notebooks/upstream/config/moduleFederation.js
@@ -10,7 +10,6 @@ const moduleFederationConfig = {
     'react-dom': { singleton: true, eager: true, requiredVersion: deps['react-dom'] },
     'react-router': { singleton: true, eager: true, requiredVersion: deps['react-router'] },
     'react-router-dom': { singleton: true, eager: true, requiredVersion: deps['react-router-dom'] },
-    '@patternfly/react-core': { singleton: true, requiredVersion: deps['@patternfly/react-core'] },
     '@openshift/dynamic-plugin-sdk': {
       singleton: true,
       requiredVersion: '*',
